eb800513da331031a589c3a2f778af6350b6bc85
Monira <ammy2029@gmail.com>
2023-11-29 16:57:00 -0500

    first commit

---
+++ src/main/java/Features/JenkinswithFranklin.java
@@ -0,0 +1,14 @@
+package Features;
+
+public class JenkinswithFranklin {
+
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("Amy will get job by december");
+
+	}
+	
+	public static void main(String[] args) {
+	System.out.println("Amy will get job by December");
+	}
+}
